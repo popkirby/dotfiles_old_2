8af361d9d24b79b64c061c7dc9e0cc64379be181
popkirby <popkirby@gmail.com>
2016-10-02 21:55:17 +0900

    zsh aliases

--- .zsh/20_completion.zsh
+++
@@ -1,9 +0,0 @@
-zstyle ':completion:*:default' menu select=2 
-
-# completion grouping
-zstyle ':completion:*:options' description 'yes'
-zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'
-zstyle ':completion:*' group-name ''
-
-# coloring
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
